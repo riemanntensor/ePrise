07028ad89f93e8d145e52f766c005e1f2ab116eb
Christian Baez <mchristianbaez@gmail.com>
2015-02-15 10:40:35 -0500

    initial commit
    initial commit of ePrise project

---
+++ ePrise/ePriseDatabaseModel.edmx.sql
@@ -0,0 +1,143 @@
+
+-- --------------------------------------------------
+-- Entity Designer DDL Script for SQL Server 2005, 2008, 2012 and Azure
+-- --------------------------------------------------
+-- Date Created: 02/13/2015 16:20:12
+-- Generated from EDMX file: C:\Users\Christian\Documents\GitHub\ePrise\ePrise\ePrise\ePriseDatabaseModel.edmx
+-- --------------------------------------------------
+
+SET QUOTED_IDENTIFIER OFF;
+GO
+USE [ePrise];
+GO
+IF SCHEMA_ID(N'dbo') IS NULL EXECUTE(N'CREATE SCHEMA [dbo]');
+GO
+
+-- --------------------------------------------------
+-- Dropping existing FOREIGN KEY constraints
+-- --------------------------------------------------
+
+
+-- --------------------------------------------------
+-- Dropping existing tables
+-- --------------------------------------------------
+
+
+-- --------------------------------------------------
+-- Creating all tables
+-- --------------------------------------------------
+
+-- Creating table 'Items'
+CREATE TABLE [dbo].[Items] (
+    [ItemId] int IDENTITY(1,1) NOT NULL,
+    [ItemName] nvarchar(max)  NOT NULL,
+    [ItemCost] decimal(18,0)  NOT NULL,
+    [VendorVendorId] int  NOT NULL
+);
+GO
+
+-- Creating table 'Customers'
+CREATE TABLE [dbo].[Customers] (
+    [CustomerId] int IDENTITY(1,1) NOT NULL,
+    [CustomerFirstName] nvarchar(max)  NOT NULL,
+    [CustomerLastName] nvarchar(max)  NOT NULL
+);
+GO
+
+-- Creating table 'Sales'
+CREATE TABLE [dbo].[Sales] (
+    [SaleId] int IDENTITY(1,1) NOT NULL,
+    [ItemSalePrice] nvarchar(max)  NOT NULL,
+    [SaleDate] datetime  NOT NULL,
+    [CustomerCustomerId] int  NOT NULL,
+    [ItemItemId] int  NOT NULL
+);
+GO
+
+-- Creating table 'Vendors'
+CREATE TABLE [dbo].[Vendors] (
+    [VendorId] int IDENTITY(1,1) NOT NULL,
+    [VendorName] nvarchar(max)  NOT NULL
+);
+GO
+
+-- --------------------------------------------------
+-- Creating all PRIMARY KEY constraints
+-- --------------------------------------------------
+
+-- Creating primary key on [ItemId] in table 'Items'
+ALTER TABLE [dbo].[Items]
+ADD CONSTRAINT [PK_Items]
+    PRIMARY KEY CLUSTERED ([ItemId] ASC);
+GO
+
+-- Creating primary key on [CustomerId] in table 'Customers'
+ALTER TABLE [dbo].[Customers]
+ADD CONSTRAINT [PK_Customers]
+    PRIMARY KEY CLUSTERED ([CustomerId] ASC);
+GO
+
+-- Creating primary key on [SaleId] in table 'Sales'
+ALTER TABLE [dbo].[Sales]
+ADD CONSTRAINT [PK_Sales]
+    PRIMARY KEY CLUSTERED ([SaleId] ASC);
+GO
+
+-- Creating primary key on [VendorId] in table 'Vendors'
+ALTER TABLE [dbo].[Vendors]
+ADD CONSTRAINT [PK_Vendors]
+    PRIMARY KEY CLUSTERED ([VendorId] ASC);
+GO
+
+-- --------------------------------------------------
+-- Creating all FOREIGN KEY constraints
+-- --------------------------------------------------
+
+-- Creating foreign key on [CustomerCustomerId] in table 'Sales'
+ALTER TABLE [dbo].[Sales]
+ADD CONSTRAINT [FK_SaleCustomer]
+    FOREIGN KEY ([CustomerCustomerId])
+    REFERENCES [dbo].[Customers]
+        ([CustomerId])
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+GO
+
+-- Creating non-clustered index for FOREIGN KEY 'FK_SaleCustomer'
+CREATE INDEX [IX_FK_SaleCustomer]
+ON [dbo].[Sales]
+    ([CustomerCustomerId]);
+GO
+
+-- Creating foreign key on [ItemItemId] in table 'Sales'
+ALTER TABLE [dbo].[Sales]
+ADD CONSTRAINT [FK_SaleItem]
+    FOREIGN KEY ([ItemItemId])
+    REFERENCES [dbo].[Items]
+        ([ItemId])
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+GO
+
+-- Creating non-clustered index for FOREIGN KEY 'FK_SaleItem'
+CREATE INDEX [IX_FK_SaleItem]
+ON [dbo].[Sales]
+    ([ItemItemId]);
+GO
+
+-- Creating foreign key on [VendorVendorId] in table 'Items'
+ALTER TABLE [dbo].[Items]
+ADD CONSTRAINT [FK_ItemVendor]
+    FOREIGN KEY ([VendorVendorId])
+    REFERENCES [dbo].[Vendors]
+        ([VendorId])
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+GO
+
+-- Creating non-clustered index for FOREIGN KEY 'FK_ItemVendor'
+CREATE INDEX [IX_FK_ItemVendor]
+ON [dbo].[Items]
+    ([VendorVendorId]);
+GO
+
+-- --------------------------------------------------
+-- Script has ended
+-- --------------------------------------------------
